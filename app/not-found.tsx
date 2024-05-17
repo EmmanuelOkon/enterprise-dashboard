1f6fa4ae2c8748fc541543192244f691d9e38bbb
Emmanuel Okon <emmaudo27@gmail.com>
2024-05-17 11:31:27 +0100

    added custom error page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import { URLS } from "@/utils/urls";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="">
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href={URLS.HOME}>Return Home</Link>
+    </div>
+  );
+}
